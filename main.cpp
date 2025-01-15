c0d031a3f4fb77720194b97fac33fa822748f6cf
Jimmyjohn23 <Jordanmckoy449@gmail.com>
2025-01-15 01:44:57 -0500

    Updated main.cpp Updated test

--- main.cpp
+++ main.cpp
@@ -5,17 +5,32 @@
 #include <cstdlib>
 
 int main() {
+    // Declare variables
     int playMoney;
     int playAgain;
+    
+    // Welcome message and prompt user to enter 10 to play
+        std::cout << "Welcome to the Slot Machine! Enter 10 to play." << std::endl;
+        std::cin >> playMoney;
 
-    std::cout << "Welcome to the Slot Machine! Enter 10 to play." << std::endl;
-    std::cin >> playMoney;
+    // Check if user entered 10 to play
+        if (playMoney == 10) {
+        std::cout << "You have $" << playMoney << " to play with." << std::endl;
+        std::cout << "Good luck! Press enter to continue" << std::endl;
+        
+        } else {
+        std::cout << "You must enter 10 to play." << std::endl;
 
+        return 0;
+        }
+    
+    // Play the game
     do{
         playMoney -= 1;
         std::srand(std::time(0));
         const char letters[4] = {'$', '7', '#', 'X'};
     
+    // Generate random numbers
         int RandIndex = rand() % 4;
         char Num0 = letters[RandIndex];
 
@@ -27,15 +42,17 @@ int main() {
     
         char output[] = {Num0, Num1, Num2};
     
+    // Display the random numbers
         for (int i = 0; i < 3; ++i) {
         std::cout << output[i];
         }
         std::cout << std::endl;
 
-        if (Num0 == Num1 && Num1 == Num2 && Num0 == '#') {
+    // Check if user wins or loses and update the amount of money
+        if (Num0 == Num1 && Num1 == Num2 && Num0 == '#') {   
         std::cout << "Congratulations! You win!" << std::endl;
         playMoney += 25; 
-        } else if (Num0 == Num1 && Num1 == Num2 && Num0 == '7') {
+         } else if (Num0 == Num1 && Num1 == Num2 && Num0 == '7') {
         std::cout << "Congratulations! You win!" << std::endl;
         playMoney += 100;
         } else if (Num0 == Num1 && Num1 == Num2 && Num0 == '$') {
@@ -47,13 +64,16 @@ int main() {
         } else{
         std::cout << "Sorry, you lose!" << std::endl;
         }
+
+    // Display the amount of money left and ask user if they want to play again
         std::cout << "You have $" << playMoney << " left." << std::endl;
         std::cout << "Would you like to play again? Enter 1 for yes or 0 for no." << std::endl;
         std::cin >> playAgain;
 
     } while (playMoney > 0 && playAgain == 1);
 
-    std::cout << "Goodbye!" << std::endl;
+    // Display goodbye message if user is out of moneyS
+    std::cout << "You're out of money. Goodbye!" << std::endl;
 
     return 0;
 }
