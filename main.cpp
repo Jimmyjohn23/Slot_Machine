47bf1e7bd081fdf3be320267b0fe23b1ae1b23ec
Jimmyjohn23 <Jordanmckoy449@gmail.com>
2025-01-16 23:54:58 -0500

    New program

--- main.cpp
+++ main.cpp
@@ -4,81 +4,97 @@
 #include <ctime>
 #include <cstdlib>
 
-int main() {
-    // Declare variables
-    int playMoney;
-    int playAgain;
+using namespace std;
+
+// Function prototypes
+char randomChar(int randomIndex, char Letters[]);
+char randomchar1(int randomIndex1, char Letters[]);
+char randomchar2(int randomIndex2, char Letters[]);
+bool iWon(char Num1, char Num2, char Num3);
+
+int main(){
+    // Seed the random number generator
+    srand(time(0));
     
-    // Welcome message and prompt user to enter 10 to play
-        std::cout << "Welcome to the Slot Machine! Enter 10 to play." << std::endl;
-        std::cin >> playMoney;
-
-    // Check if user entered 10 to play
-        if (playMoney == 10) {
-        std::cout << "You have $" << playMoney << " to play with." << std::endl;
-        std::cout << "Good luck! Press enter to continue" << std::endl;
-        
-        } else {
-        std::cout << "You must enter 10 to play." << std::endl;
-
+    // Welcome message and instructions
+    int playerMoney;
+    bool playAgain = 1;
+    cout << "Welcome to the slot machine game!" << endl;
+    cout << "Enter $10 to play the game!" << endl;
+    cin >> playerMoney;
+    
+    // Check if the player has enough money to play the game
+    bool money = playerMoney == 10;
+    if (money){
+        cout << "You have $" << playerMoney << " left." << endl;
+    }
+    else{
+        cout << "Invaild amount of money." << endl;
         return 0;
-        }
-    
-    // Play the game
-    do{
-        playMoney -= 1;
-        std::srand(std::time(0));
-        const char letters[4] = {'$', '7', '#', 'X'};
-    
-    // Generate random numbers
-        int RandIndex = rand() % 4;
-        char Num0 = letters[RandIndex];
-
-        int RandIndex1 = rand() % 4;
-        char Num1 = letters[RandIndex1];
-    
-        int RandIndex2 = rand() % 4;
-        char Num2 = letters[RandIndex2];
-    
-        char output[] = {Num0, Num1, Num2};
-    
-    // Display the random numbers
-        for (int i = 0; i < 3; ++i) {
-        std::cout << output[i];
-        }
-        std::cout << std::endl;
-
-    // Check if user wins or loses and update the amount of money
-        if (Num0 == Num1 && Num1 == Num2 && Num0 == '#') {   
-        std::cout << "Congratulations! You win!" << std::endl;
-        playMoney += 25; 
-         } else if (Num0 == Num1 && Num1 == Num2 && Num0 == '7') {
-        std::cout << "Congratulations! You win!" << std::endl;
-        playMoney += 100;
-        } else if (Num0 == Num1 && Num1 == Num2 && Num0 == '$') {
-        std::cout << "Congratulations! You win!" << std::endl;
-        playMoney += 50;
-        } else if (Num0 == Num1 && Num1 == Num2 && Num0 == 'X') {
-        std::cout << "Congratulations! You win!" << std::endl;
-        playMoney += 10;
-        } else{
-        std::cout << "Sorry, you lose!" << std::endl;
-        }
-
-    // Display the amount of money left and ask user if they want to play again
-        std::cout << "You have $" << playMoney << " left." << std::endl;
-        std::cout << "Would you like to play again? Enter 1 for yes or 0 for no." << std::endl;
-        std::cin >> playAgain;
-
-    } while (playMoney > 0 && playAgain == 1);
-
-    // Display goodbye message 
-    if (playMoney == 0){
-        std::cout << "You have no money left. Goodbye!" << std::endl;
-    } else {
-        std::cout << "You have won: $" << playMoney << std::endl;
-        std::cout << "Goodbye!" << std::endl;
     }
 
+    // Slot machine game
+    do{
+        char Num1, Num2, Num3;
+        char Letters[] = {'$', 'X', '7', '#'};
+        Num1 = randomChar(0, Letters);
+        Num2 = randomchar1(0, Letters);
+        Num3 = randomchar2(0, Letters);
+        playerMoney -= 1;
+
+        cout << "The slot machine shows: " << Num1 << Num2 << Num3 << endl;
+        if (iWon(Num1, Num2, Num3)&& Num1 == 'X'){
+            playerMoney += 10;
+            cout << "You won $10!" << endl;
+        
+        }
+         else if (iWon(Num1, Num2, Num3)&& Num1 == '7'){
+            playerMoney += 100;
+            cout << "You won $100!" << endl;
+        
+        }
+        else if (iWon(Num1, Num2, Num3)&& Num1 == '#'){
+            playerMoney += 25;
+            cout << "You won $25!" << endl;
+    
+        }
+        else if (iWon(Num1, Num2, Num3)&& Num1 == '$'){
+            playerMoney += 50;
+            cout << "You won $50!" << endl;
+         }
+        else{
+            cout << "You lost!" << endl;
+        }
+        cout << "You have $" << playerMoney << " left." << endl;
+        cout << "Do you want to play again? (Yes = 1, No = 0)" << endl;
+        cin >> playAgain;
+    } while (playerMoney > 0 && playAgain );
+
+    cout << "You have $" << playerMoney << " left." << endl;
+    cout << "Thank you for playing!" << endl;   
     return 0;
+}
+// Function definitions
+char randomChar(int randomIndex, char Letters[]){
+    randomIndex = rand() % 4;
+    char randomChar = Letters[randomIndex];
+    return randomChar;
+}
+char randomchar1(int randomIndex1, char Letters[]){
+    randomIndex1 = rand() % 4;
+    char randomChar1 = Letters[randomIndex1];
+    return randomChar1;
+}
+char randomchar2(int randomIndex2, char Letters[]){
+    randomIndex2 = rand() % 4;
+    char randomChar2 = Letters[randomIndex2];
+    return randomChar2;
+}
+bool iWon(char Num1, char Num2, char Num3){
+    if(Num1 == Num2 && Num2 == Num3){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
